5a7a09b52d4e98835b7429e4a1e61652ef4d43df
haliko <halilmertdeveliii@gmail.com>
2023-07-19 00:53:00 +0300

    it is working and it is maven project and it workds

---
+++ src/main/java/com/example/trySomething/TrySomethingApplication.java
@@ -0,0 +1,13 @@
+package com.example.trySomething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySomethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrySomethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trySomething/TrySomethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trySomething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrySomethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
