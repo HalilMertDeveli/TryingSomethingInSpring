1bb550d188b68b6feee9b306b71603f2d6aa6360
haliko <halilmertdeveliii@gmail.com>
2023-07-19 11:28:55 +0300

    i wrote that getting part and i will write to codding part

--- src/main/java/com/example/trySomething/TrySomethingApplication.java
+++ src/main/java/com/example/trySomething/TrySomethingApplication.java
@@ -3,6 +3,10 @@ package com.example.trySomething;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 @SpringBootApplication
 public class TrySomethingApplication {
 
@@ -11,3 +15,34 @@ public class TrySomethingApplication {
 	}
 
 }
+ class FakePersonDataAccessService implements PersonOperationInterface{
+	private static List<PersonModelClass> fakeDatabase =new ArrayList<>();
+	 @Override
+	 public int insertPerson(UUID userId, PersonModelClass personInstance) {
+		 fakeDatabase.add(new PersonModelClass(userId,personInstance.getUserName()));
+		 return 1;
+	 }
+ }
+ interface PersonOperationInterface{
+	int insertPerson(UUID userId,PersonModelClass personInstance);
+	default int insertPerson(PersonModelClass personInstance){
+		UUID generatedId = UUID.randomUUID();
+		return insertPerson(generatedId,personInstance);
+	}
+ }
+ class PersonModelClass{
+	final  UUID userId;
+	final String userName;
+
+
+	 PersonModelClass(UUID id, String userName) {
+		 this.userId = id;
+		 this.userName = userName;
+	 }
+	public UUID getUserId(){
+		 return userId;
+	}
+	public String getUserName(){
+		 return userName;
+	}
+ }
